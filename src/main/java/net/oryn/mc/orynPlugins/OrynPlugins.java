3775f345044f1fb726190b644d8c45e9ec2d8c8e
Fahry-a <farhannzarm@gmail.com>
2026-06-19 09:57:56 +0700

    init

---
+++ src/main/java/net/oryn/mc/orynPlugins/OrynPlugins.java
@@ -0,0 +1,16 @@
+package net.oryn.mc.orynPlugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OrynPlugins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
