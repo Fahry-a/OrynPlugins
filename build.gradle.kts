fb4ae48c17e904950645db43793af0cf8522f73f
Fahry-a <farhannzarm@gmail.com>
2026-06-19 12:39:47 +0700

    add maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
     id("com.gradleup.shadow") version "9.4.2"
 }
 
+group = "net.oryn.mc"
+version = "1.0.0"
+description = "A Main Plugin For Oryn Server"
+
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
@@ -22,24 +26,31 @@ java {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = "orynplugins"
+            version = project.version.toString()
+
+            artifact(tasks.shadowJar)
 
             pom {
                 name.set("OrynPlugins")
                 description.set("A Main Plugin For Oryn Server")
                 url.set("https://github.com/Fahry-a/OrynPlugins")
+
                 licenses {
                     license {
                         name.set("MIT License")
                         url.set("https://opensource.org/licenses/MIT")
                     }
                 }
+
                 developers {
                     developer {
                         id.set("Fahry-a")
                         name.set("Fahry-a")
                     }
                 }
+
                 scm {
                     connection.set("scm:git:git://github.com/Fahry-a/OrynPlugins.git")
                     developerConnection.set("scm:git:ssh://github.com/Fahry-a/OrynPlugins.git")
@@ -48,26 +59,28 @@ publishing {
             }
         }
     }
+
     repositories {
         maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Fahry-a/OrynPlugins")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
+            name = "LocalRepo"
+            url = uri(layout.buildDirectory.dir("repo"))
         }
     }
 }
 
 tasks {
+
     runServer {
         minecraftVersion("1.21.1")
         jvmArgs("-Xms2G", "-Xmx2G")
     }
 
     processResources {
-        val props = mapOf("version" to version, "description" to project.description)
+        val props = mapOf(
+            "version" to project.version,
+            "description" to project.description
+        )
+
         filesMatching("plugin.yml") {
             expand(props)
         }
@@ -77,7 +90,15 @@ tasks {
         archiveClassifier.set("")
     }
 
+    jar {
+        enabled = false
+    }
+
     build {
+        dependsOn(shadowJar)
+    }
+
+    publish {
         dependsOn(shadowJar)
     }
 }
